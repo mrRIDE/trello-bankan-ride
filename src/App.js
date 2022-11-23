a5c26e03be9849e634b477d7911d97496fa569cc
Mr Rider <thaithanhspkt@hotmail.com>
2022-11-23 13:06:00 +0900

    Design UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import "./App.scss";
 import React from "react";
-import ReactDOM from "react-dom";
 
 function App() {
   return (
     <div className="trello-ride-master">
-      <div className="navbar-app">App bar</div>
-      <div className="navbar-board">Board bar</div>
-      <div className="board-columns">Board column</div>
+      <div className="navbar app">App bar</div>
+      <div className="navbar board">Board bar</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-itm1-1.xx.fbcdn.net/v/t1.6435-9/95594469_2645052842446731_8755693851152220160_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=rhJV_IQ8xosAX94_CpA&_nc_ht=scontent-itm1-1.xx&oh=00_AfDrgdmlrjKGZu1uHO-532F9szGh3Dkxs47xXktVvDtsCQ&oe=63A4EDF3"
+                alt="thanh-ride-img"
+              />
+              Title: Than Thanh Dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>Add another Line</footer>
+        </div>
+        <div className="column">
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent-itm1-1.xx.fbcdn.net/v/t1.6435-9/95594469_2645052842446731_8755693851152220160_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=rhJV_IQ8xosAX94_CpA&_nc_ht=scontent-itm1-1.xx&oh=00_AfDrgdmlrjKGZu1uHO-532F9szGh3Dkxs47xXktVvDtsCQ&oe=63A4EDF3"
+                alt="thanh-ride-img"
+              />
+              Title: Than Thanh Dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>Add another Line</footer>
+        </div>
+      </div>
     </div>
   );
 }
